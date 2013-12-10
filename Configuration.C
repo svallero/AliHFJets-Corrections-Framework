05fbff6c1780bec9fa7a2a55783a65d2c735b809
svallero <svallero@to.infn.it>
2013-12-10 13:22:17 +0100

    Firts commit of general container class.

---
+++ Configuration.C
@@ -0,0 +1,49 @@
+TString trainName = "HFCJ";
+Long64_t nentries=123567890,firstentry=0;
+TString loadMacroPath="$ALICE_ROOT/PWGHF/vertexingHF/macros/";
+
+// GRID ---------------------------
+TString user="svallero";
+TString pluginmode="test";
+TString name="AnalysisBjets";
+// packages
+TString api="V1.1x";
+TString root="v5-34-08";
+TString aliroot="v5-05-36-AN";
+TString boost="boost::v1_51_0";
+TString cgal="cgal::v4.0.2";
+TString fastjet="fastjet::v3.0.3";
+// data
+TString datadir="/alice/sim/2013/LHC13d18d";
+TString datapattern="AOD/*AliAOD.root";
+TString runs="126082";
+
+
+// --- include path --------------- 
+TString includepath=("-I. -I$ROOTSYS/include -I$ALICE_ROOT -I$ALICE_ROOT/include -I$ALICE_ROOT/ITS -I$ALICE_ROOT/TPC -I$ALICE_ROOT/CONTAINERS -I$ALICE_ROOT/STEER/STEER -I$ALICE_ROOT/STEER/STEERBase -I$ALICE_ROOT/STEER/ESD -I$ALICE_ROOT/STEER/AOD -I$ALICE_ROOT/TRD -I$ALICE_ROOT/macros -I$ALICE_ROOT/ANALYSIS  -I$ALICE_ROOT/OADB -I$ALICE_ROOT/PWGHF -I$ALICE_ROOT/PWGHF/base -I$ALICE_ROOT/PWGHF/vertexingHF -I$ALICE_ROOT/PWG/FLOW/Base -I$ALICE_ROOT/PWG/FLOW/Tasks -I$ALICE_ROOT/JETAN/fastjet -I$ALICE_ROOT/JETAN/fastjet/fastjet -I$ALICE_ROOT/JETAN -I$ALICE_ROOT/PWG/Tools -g"); 
+
+// --- libraries ------------------
+TString local_fastjet_path="/usr/local/lib";
+TString loaddylibs=("libCGAL libfastjet libsiscone libsiscone_spherical libfastjetplugins");
+TString loadlibs=("libJETAN libFASTJETAN libPWGTools libPWGflowBase libPWGflowTasks libPWGmuon libPWGHFbase libPWGHFvertexingHF libFASTJETAN libPWGJE");
+
+// --- analysis sources -----------
+TString sources=("AliHFJetContainer AliRDHFJetsCuts AliRDHFJetsCutsVertex AliHFJetVertexTagging AliAnalysisTaskSEHFJets");
+
+
+void AddTasks(){
+
+  // JetFinder 
+  Int_t kHighPtFilterMask = 272;
+  UInt_t iPhysicsSelectionFlag = AliVEvent::kAny; 
+  Float_t fTrackEtaWindow = 0.9;  
+
+  gROOT->LoadMacro("AddTaskJetCluster.C");
+  AliAnalysisTaskJetCluster *taskCl = 0;
+  taskCl = AddTaskJetCluster("AOD","",kHighPtFilterMask,iPhysicsSelectionFlag,"ANTIKT",0.4,0,kTRUE,"AliAOD.Jets.root",0.15,fTrackEtaWindow);
+
+  // BTagging 
+  gROOT->LoadMacro("AddTaskSEHFJets.C");
+  AliAnalysisTaskSEHFJets* taskJet=AddTaskSEHFJets("standard",2,"","c",0);
+
+}
---
+++ RunTest.C
@@ -0,0 +1,97 @@
+void RunTest()
+{
+
+  // Load configuration
+  gROOT->LoadMacro("TestConfiguration.C");
+
+  // Set paths
+  gSystem->SetIncludePath(includepath.Data()); 
+  gSystem->AddDynamicPath(local_fastjet_path.Data()); 
+
+  // Load libraries locally 
+  TString loadLibraries="LoadLibraries.C"; loadLibraries.Prepend(loadMacroPath.Data());
+  gROOT->LoadMacro(loadLibraries.Data());
+  LoadLibraries(kFALSE);
+  // for FastJet
+  TObjArray *arr;
+  TObjString *objstr;
+  arr = loaddylibs.Tokenize(" ");
+  TIter next(arr);
+  while ((objstr=(TObjString*)next())){
+    TString str=(objstr->GetString()).Append(".dylib");
+    Printf("Loading %s", str.Data());
+    gSystem->Load(str.Data());
+  }
+  // standard AliRoot
+  arr = loadlibs.Tokenize(" ");
+  TIter next(arr);
+  while ((objstr=(TObjString*)next())){
+    TString str=objstr->GetString();
+    Printf("Loading %s", str.Data());
+    gSystem->Load(str.Data());
+  }
+
+  // Load analysis sources locally
+  arr = sources.Tokenize(" ");
+  TIter next(arr);
+  while ((objstr=(TObjString*)next())){
+    TString str=(objstr->GetString()).Append(".cxx++g");
+    gROOT->LoadMacro(str.Data());
+  }
+
+  if (1){
+    const Int_t nvars = 3;
+    Int_t nbins[nvars]={3,3,3};
+    Double_t *binning[nvars];
+    Double_t bins1[nvars+1]={0.,1.,2.,3.};
+    Double_t bins2[nvars+1]={0.,1.,2.,3.};
+    Double_t bins3[nvars+1]={0.,1.,2.,3.};
+    binning[0]=bins1;
+    binning[1]=bins2;
+    binning[2]=bins3;
+    const char* axistitle[nvars];
+    axistitle[0]="Primo asse";
+    axistitle[1]="Secondo asse";
+    axistitle[2]="Terzo asse";
+    const char* axisname[nvars];
+    axisname[0]="axis1";
+    axisname[1]="axis2";
+    axisname[2]="axis3";
+
+    AliHFJetContainer *cont= new AliHFJetContainer("Dummy",nvars, axisname, nbins, binning, axistitle);
+    //Double_t point[7]={5.,7., 0.2, 0.2, 1.5,1.5,1.5}; 
+    //cont->Fill(point);
+    //cont->PrintSteps();
+    //cont->PrintVars();
+    //cont->SetAxisRangeStep("kCFJetPt", 10., 20., AliHFJetContainer::kCFStepAll);
+    cont->SetAxisRangeAllSteps("kCFJetPt", 10., 20.);
+    //cont->ResetAxisStep("axis1", AliHFJetContainer::kCFStepAll);
+    //cont->ResetAxisAllSteps("axis1");
+    //TH1D *h = cont->Project1D(AliHFJetContainer::kCFStepAll, "axis1");
+    //h->DrawCopy();
+    
+    TH2D *heff=cont->GetEfficiency2D();
+    new TCanvas();
+    heff->DrawCopy();
+  } else {      
+    AliHFJetContainerVertex *cont= new AliHFJetContainerVertex("Dummy");
+    //new TCanvas();
+    //cont->ResetAxisStep(AliHFJetContainer::kCFMult, AliHFJetContainer::kCFStepAll);
+    //TH1D *h2 = cont->Project1D(AliHFJetContainer::kCFStepAll, AliHFJetContainer::kCFMult);
+    //h2->DrawCopy();
+
+    // Vecchia versione
+    //AliCFContainer *cf2 =(AliCFContainer*)cont->GetContainerPrimVtx();
+    //Double_t point[13]={0.5,0.5,0.5,3,0,1,2,3,0.5,0.1,0.1,0.1,0.5};
+    //if (cf1)
+    //  cf1->Fill(point,0);
+    //else Printf("cf1 does not exist!!!");
+    //TFile *f=new TFile("tmp.root","RECREATE");
+    //cf->Write();
+    //f->Close();
+  }
+  return;
+}
+
+
+
